47c584ae12385d7a263caab8c2bf4324d0689bfe
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-10-19 13:37:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,26 +10,26 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("", "")
+TG_BOT_TOKEN = os.environ.get("6285168124:AAHm4WK3H0jNWtbWlTrWVnEmgAH2j4CArbE")
 
 #Your API ID from my.telegram.org
-APP_ID = int(os.environ.get("APP_ID", ""))
+APP_ID = int(os.environ.get("20120089"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("dfb3ac85e07f353be8d09110e189a6e0")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
+CHANNEL_ID = int(os.environ.get("-1001736005127"))
 
 #OWNER ID
-OWNER_ID = int(os.environ.get("5005777092", ""))
+OWNER_ID = int(os.environ.get("5005777092", ))
 
 #Port
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Devil")
+DB_URI = os.environ.get("mongodb+srv://Devil:<8dTwwn8LFba8dyj>@cluster0.7frtspj.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("Davil")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = os.environ.get("FORCE_SUB_CHANNEL", "0")
