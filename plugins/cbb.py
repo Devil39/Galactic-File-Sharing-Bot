70ac59822f4508231e09dd53ec827876b9a4161e
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-10-20 18:02:24 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -12,12 +12,12 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○╭━━━━━━━━━━━━━━━➣
-                   f"\n  ┣⪼Creator : <a href='tg://user?id={OWNER_ID}'>Me</a>
-                    f"\n○┣⪼Language : <code>Python3</code>
-                    f"\n○┣⪼Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {version}</a>
-                    f"\n○┣⪼Channel : @Anime_Galactic
-                    f"\n ╰━━━━━━━━━━━━━━━</b>
+            text = f"<b>○"╭━━━━━━━━━━━━━━━➣"
+                   f"\n  "┣⪼"Creator : <a href='tg://user?id={OWNER_ID}'>Me</a>
+                    f"\n○"┣⪼"Language : <code>Python3</code>
+                    f"\n○"┣⪼"Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {version}</a>
+                    f"\n○"┣⪼"Channel : @Anime_Galactic
+                    f"\n "╰━━━━━━━━━━━━━━━"</b>
          ",   
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
