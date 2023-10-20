b064294b2bda6e6ab07424abde14db96b8e50eb4
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-10-20 09:56:08 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -19,7 +19,6 @@ async def cb_handler(client: Bot, query: CallbackQuery):
                 ]
             )
         )
-        )
     elif data == "close":
         await query.message.delete()
         try:
