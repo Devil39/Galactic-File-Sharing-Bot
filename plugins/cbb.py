ecda3a0f4591f6e23a102f26c1608dd4532c2813
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-10-20 16:14:44 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,12 +10,18 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @Anime_Galactic</b>",   
+            text = f"<b>○ 
+            ╭━━━━━━━━━━━━━━━➣
+            ┣⪼Creator : <a href='tg://user?id={OWNER_ID}'>Me
+            ┣⪼</a>\n○ Language : <code>Python3</code>
+            ┣⪼\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}
+            ┣⪼</a>\n○ Channel : @Anime_Galactic</b>
+            ╰━━━━━━━━━━━━━━━➣",   
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                    [
-                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                        InlineKeyboardButton("𝑪𝒍𝒐𝒔𝒆", callback_data = "close")
                    ]    
                 ]
             )
