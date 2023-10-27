fd2d1ec0b1b98223f8ca79fde3012709d388f677
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-10-27 12:39:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,8 @@ START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store privat
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(5137934602))
+        ADMINS.append(int(5137934602
+                          2144648658 ))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
@@ -61,7 +62,7 @@ BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
 USER_REPLY_TEXT = "❌Don't send me messages directly I'm only File Share bot!"
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(5005777092 , 2144648658)
+ADMINS.append(5005777092)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
