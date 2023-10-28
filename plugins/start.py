c7436adc5d88c6a9c0ebfac67372d66ff054dded
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-10-28 13:32:39 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -93,6 +93,9 @@ async def start_command(client: Client, message: Message):
                 ]          
             ]
         )
+        if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC)
+                                  
         await message.reply_text(
             text = START_MSG.format(
                 first = message.from_user.first_name,
