32539da0390325eb06e90c39c335e2c7fb2244c0
DEVIL <132729234+Devil39@users.noreply.github.com>
2023-11-05 00:17:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store privat
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
-        ADMINS.append(int(5137934602  2144648658 ))
+        ADMINS.append(int(5137934602 , 2144648658 ))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
